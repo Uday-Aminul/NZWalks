f1d83b84d60477a10ad29df06feb2a4288e5eacc
Uday Aminul <udayaminul20@gmail.com>
2025-06-20 18:51:29 +0600

    Created the repository for Walks table

--- Repositories/IWalkRepository.cs
+++ Repositories/IWalkRepository.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NZWalks.Models.Domain;
 
 namespace NZWalks.Repositories
 {
     public interface IWalkRepository
     {
-        
+        Task<List<Walk>> GetAllWalkAsync();
+        Task<Walk?> GetWalkByIdAsync(Guid id);
+        Task<Walk> CreateWalkAsync(Walk walk);
+        Task<Walk?> UpdateWalkAsync(Guid id, Walk walk);
+        Task<List<Walk>?> DeleteWalkAsync(Guid id);
     }
 }
--- Repositories/SQLWalkRepository.cs
+++ Repositories/SQLWalkRepository.cs
@@ -2,11 +2,72 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.Data;
+using NZWalks.Models.Domain;
 
 namespace NZWalks.Repositories
 {
-    public class SQLWalkRepository:IWalkRepository
+    public class SQLWalkRepository : IWalkRepository
     {
+        private readonly NZWalksDbContext dbContext;
+        public SQLWalkRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext=dbContext;
+        }
         
+        public async Task<Walk> CreateWalkAsync(Walk walk)
+        {
+            await dbContext.Walks.AddAsync(walk);
+            await dbContext.SaveChangesAsync();
+            return walk;
+        }
+
+        public async Task<List<Walk>?> DeleteWalkAsync(Guid id)
+        {
+            var existingWalk = await dbContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingWalk is null)
+            {
+                return null;
+            }
+            dbContext.Walks.Remove(existingWalk);
+            await dbContext.SaveChangesAsync();
+            var walksDomain = await dbContext.Walks.ToListAsync();
+            return walksDomain;
+        }
+
+        public async Task<List<Walk>> GetAllWalkAsync()
+        {
+            var walksDomain = await dbContext.Walks.ToListAsync();
+            return walksDomain;
+        }
+
+        public async Task<Walk?> GetWalkByIdAsync(Guid id)
+        {
+            var existingWalk = await dbContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingWalk is null)
+            {
+                return null;
+            }
+            return existingWalk;
+        }
+
+        public async Task<Walk?> UpdateWalkAsync(Guid id, Walk walk)
+        {
+            var existingWalk = await dbContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingWalk is null)
+            {
+                return null;
+            }
+            existingWalk.Difficulty = walk.Difficulty;
+            existingWalk.WalkImageUrl = walk.WalkImageUrl;
+            existingWalk.LengthInKm = walk.LengthInKm;
+            existingWalk.Description = walk.Description;
+            existingWalk.Name = walk.Name;
+            existingWalk.DifficultyId = walk.DifficultyId;
+            existingWalk.RegionId = walk.RegionId;
+            await dbContext.SaveChangesAsync();
+            return existingWalk;
+        }
     }
 }
