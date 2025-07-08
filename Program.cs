721be5c3ef81a5431cb93fa7b6c5d66b6458c6be
Uday Aminul <udayaminul20@gmail.com>
2025-07-08 21:16:17 +0600

    Added middlewire to use Custom folder for keeping static files.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using NZWalks.Data;
 using NZWalks.Mappings;
 using NZWalks.Repositories;
@@ -34,7 +35,11 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider =new PhysicalFileProvider(Path.Combine(builder.Environment.WebRootPath, "/Images")),
+    RequestPath="/Images"
+});
 app.MapControllers();
 
 app.Run();
