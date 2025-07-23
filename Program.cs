b3367f2dd8368b56f5819c1eb466a337c9fb598d
Uday Aminul <udayaminul20@gmail.com>
2025-07-23 19:09:12 +0600

    Added HttpContextAccessor, Serilog and required packages

--- Program.cs
+++ Program.cs
@@ -4,14 +4,22 @@ using NZWalks.Data;
 using NZWalks.Mappings;
 using NZWalks.Middlewires;
 using NZWalks.Repositories;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration().WriteTo.Console().WriteTo.File("Logs/NZWalksLogs.txt", rollingInterval: RollingInterval.Infinite).MinimumLevel.Information().CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 // Add services to the container.
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
 builder.Services.AddControllers();
+//builder.Services.AddHttpContextAccessor();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
