11b7d01895b6f86627fda99cee6dade158d6ee29
Uday Aminul <udayaminul20@gmail.com>
2025-08-01 17:10:56 +0600

    Configured Password validation in program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
 using NZWalks.Data;
 using NZWalks.Mappings;
 using NZWalks.Middlewires;
@@ -32,6 +38,37 @@ builder.Services.AddScoped<IImageRepository, LocalImageRepository>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")
+    .AddEntityFrameworkStores<NZWalksAuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 4;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options=>
+    options.TokenValidationParameters=new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -44,6 +81,7 @@ app.UseExceptionHandlerMiddlewire();
 
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
 app.UseAuthorization();
 app.UseStaticFiles(new StaticFileOptions()
 {
--- Readme.md
+++ Readme.md
@@ -2,3 +2,5 @@ The project wont normally run without a file called "appsettings.json".
 It is ignored for security reasons and u will have to add your own.
 
 Create a file named appsettings.json on the root directory so that it is a sibling of folders like Controller and Properties.
+
+The password configuration is very simple for testing purposes. You can change it to however u like.
