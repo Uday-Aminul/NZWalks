b5b374fb3534755ffc67e08f44a9d5c878715902
Uday Aminul <udayaminul20@gmail.com>
2025-08-14 09:39:13 +0600

    Not Done

---
+++ NZWalks.Test/UnitTests/Controller/RegionsControllerTests.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Moq;
+using NZWalks.Controllers;
+using NZWalks.Models.Domain;
+using NZWalks.Models.DTOs;
+using NZWalks.Repositories;
+using Xunit;
+
+namespace NZWalks.Test.UnitTests.Controller
+{
+    public class RegionsControllerTests
+    {
+        private readonly Mock<IRegionRepository> regionRepository;
+        private readonly Mock<IMapper> mapper;
+        private readonly RegionsController regionsController;
+        public RegionsControllerTests()
+        {
+            regionRepository = new Mock<IRegionRepository>();
+            mapper = new Mock<IMapper>();
+            regionsController = new RegionsController(regionRepository.Object, mapper.Object);
+        }
+
+        [Fact]
+        public async Task RegionsController_GetAll_ReturnsOk()
+        {
+            //Arrange
+            var regions = new List<Region>()
+            {
+                new Region()
+                {
+                    Id = Guid.NewGuid(),
+                    Code = "R001",
+                    Name = "Region 1",
+                },
+                new Region()
+                {
+                    Id = Guid.NewGuid(),
+                    Code = "R002",
+                    Name = "Region 2",
+                }
+            };
+            var regionDto = new List<RegionDto>()
+            {
+                new RegionDto()
+                {
+                    Id = Guid.NewGuid(),
+                    Code = "R001",
+                    Name = "Region 1",
+                },
+                new RegionDto()
+                {
+                    Id = Guid.NewGuid(),
+                    Code = "R002",
+                    Name = "Region 2"
+                }
+            };
+            regionRepository.Setup(repo=>repo.GetAllRegionAsync(1, 10))
+
+            //Act
+            var result 
+            //Assert
+
+        }
+    }
+}
